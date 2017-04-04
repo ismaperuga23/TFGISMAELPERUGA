9f12596fdc3d194e8f4a9a22df3cd662d849f8d5
Isma <ismaperuga@gmail.com>
2017-04-04 19:11:26 +0200

    Step 2, MSE has to be fixed

---
+++ plotMSE.m
@@ -0,0 +1,11 @@
+function plotMSE(MSE,BS,user)
+% This function plots the MSE
+% MSE: matrix of MSE calculated
+% BS: The number of the BS
+% user: The user in the BS
+
+figure;
+plot(MSE(BS,:,user));
+
+
+end
